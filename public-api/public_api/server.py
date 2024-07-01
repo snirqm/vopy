f6f4eff42dc9555348c48769886f66c78b6a825d
Jonathan Yerushalmi <jonathan.yerushalmi@quantum-machines.co>
2024-07-01 15:10:11 +0300

    updated the calling of the protobuf classes

--- public-api/public_api/server.py
+++ public-api/public_api/server.py
@@ -1,10 +1,10 @@
 import asyncio
-from typing import List, Dict
+from typing import Dict
 
-from gen.vopy import PublicApiStub, Empty, Status, Session, Config, SimulationInfo
+from gen import vopy
 from grpclib.server import Server
 from grpclib.exceptions import GRPCError
-import grpclib
+from grpclib.const import Status
 import itertools
 import os
 from datetime import datetime
@@ -15,7 +15,7 @@ import subprocess
 
 @dataclass
 class SessionInfo:
-    session: Session
+    session: vopy.Session
     is_build_successful: bool = False
     process: subprocess.Popen = None
 
@@ -28,19 +28,19 @@ class SessionInfo:
         return self.process is not None and self.process.poll() != 0
 
 
-class PublicService(PublicApiStub):
+class PublicService(vopy.PublicApiStub):
     def __init__(self):
         self.sessions_info_dict: Dict[int, SessionInfo] = {}
         self.key_generator = itertools.count()
         self.path_to_server_workspace = r""  # TODO: set the path to the server workspace
         self.server_dir_name = self.get_server_dir_name()
 
-    async def get_status(self, empty: Empty) -> Status:
-        return Status(open_simulations=self.get_open_simulations(),
+    async def get_status(self, empty: vopy.Empty) -> Status:
+        return vopy.Status(open_simulations=self.get_open_simulations(),
                       running_simulations=self.get_running_simulations(),
                       failed_simulations=self.get_failed_simulations())
 
-    async def create_session(self, empty: Empty) -> Session:
+    async def create_session(self, empty: vopy.Empty) -> vopy.Session:
         """
         Creates a key
         makes a directory to store the simulation files
@@ -48,12 +48,12 @@ class PublicService(PublicApiStub):
         key = next(self.key_generator)
         self.make_dir(dir_path=self.dir_path(key))
 
-        session = Session(key=key)
+        session = vopy.Session(key=key)
         session_info = SessionInfo(session=session)
         self.sessions_info_dict[key] = session_info
         return session
 
-    async def send_config(self, config: Config) -> Empty:
+    async def send_config(self, config: vopy.Config) -> vopy.Empty:
         session = config.session
         key = session.key
 
@@ -61,9 +61,9 @@ class PublicService(PublicApiStub):
 
         self.build_simulation_files()
         self.sessions_info_dict[key].is_build_successful = True
-        return Empty()
+        return vopy.Empty()
 
-    async def start_sim(self, session: Session) -> SimulationInfo:
+    async def start_sim(self, session: vopy.Session) -> vopy.SimulationInfo:
         self.check_session_exists(session.key)
         self.check_send_config_successful(session.key)
         self.check_simulation_is_not_running(session.key)
@@ -76,7 +76,7 @@ class PublicService(PublicApiStub):
                                               stderr=subprocess.PIPE)
         self.sessions_info_dict[session.key].process = simulation_session
 
-        return SimulationInfo(port=port, session=session)
+        return vopy.SimulationInfo(port=port, session=session)
 
     @staticmethod
     def dir_name(key: int) -> str:
@@ -94,7 +94,7 @@ class PublicService(PublicApiStub):
         if not os.path.exists(dir_path):
             os.mkdir(dir_path)
         else:
-            raise GRPCError(status=grpclib.const.Status.ALREADY_EXISTS,
+            raise GRPCError(status=Status.ALREADY_EXISTS,
                             message=f"There has been an error creating the directory {dir_path}, it already exists.")
 
     @staticmethod
@@ -108,17 +108,17 @@ class PublicService(PublicApiStub):
 
     def check_session_exists(self, key: int) -> None:
         if key not in self.sessions_info_dict:
-            raise GRPCError(status=grpclib.const.Status.FAILED_PRECONDITION,
+            raise GRPCError(status=Status.FAILED_PRECONDITION,
                             message=f"No such simulation with key {key}")
 
     def check_send_config_successful(self, key: int) -> None:
         if not self.sessions_info_dict[key].is_build_successful:
-            raise GRPCError(status=grpclib.const.Status.FAILED_PRECONDITION,
+            raise GRPCError(status=Status.FAILED_PRECONDITION,
                             message=f"Config was not sent successfully for simulation with key {key}")
 
     def check_simulation_is_not_running(self, key: int) -> None:
         if self.sessions_info_dict[key].is_sim_running:
-            raise GRPCError(status=grpclib.const.Status.ALREADY_EXISTS,
+            raise GRPCError(status=Status.ALREADY_EXISTS,
                             message=f"Simulation with key {key} is already running")
 
     @property
